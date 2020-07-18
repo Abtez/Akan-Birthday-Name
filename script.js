a0ebf0f0063a077f84d6e553de1477bf1376c27c
Abzed <ibraabzed@gmail.com>
2020-07-18 09:22:51 +0300

    add days of the week arrays

---
+++ script.js
@@ -0,0 +1,18 @@
+function makeArray(n){
+    this.length=n;
+    for (var i=1; i<=n; i++){
+      this[i]=0;
+    }
+  }
+
+days=new makeArray(7);
+days[o]="Saturday"
+days[1]="Sunday"
+days[2]="Monday"
+days[3]="Tuesday"
+days[4]="Wednesday"
+days[5]="Thursday"
+days[6]="Friday"
+
+months=new makeArray(12);
+months[]=""
