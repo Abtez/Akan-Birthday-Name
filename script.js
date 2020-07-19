c71f4939fff6047f868625e13fcdf9702a30d422
Abzed <ibraabzed@gmail.com>
2020-07-19 09:54:24 +0300

    add fuction to get any date and different gender

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-function
+function compute(event){
+    event.preventDefaults();
+
+    var form=new FormData(event.target);
+    var day=form.get("day");
+    var month=form.get("month");
+    var year=form.get("year");
+    var gender=form.get("gender");
+     
+}
