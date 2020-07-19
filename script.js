d359d6808dc91ae7e654e4134666ca286bf29b4c
Abzed <ibraabzed@gmail.com>
2020-07-19 10:01:55 +0300

    add function to pair day of week with akan name

--- script.js
+++ script.js
@@ -6,5 +6,40 @@ function compute(event){
     var month=form.get("month");
     var year=form.get("year");
     var gender=form.get("gender");
-     
+    var names= {
+        Sunday: {
+        male: 'Kwasi',
+        female: 'Akosua'
+        },
+        Monday: {
+        male: 'Kwadwo',
+        female: 'Adwoa'
+        },
+        Tuesday: {
+        male: 'Kwabena',
+        female: 'Abenaa'
+        },
+        Wednesday: {
+        male: 'Kwaku',
+        female: 'Akua'
+        },
+        Thursday: {
+        male: 'Yaw',
+        female: 'Yaa'
+        },
+        Friday: {
+        male: 'Kofi',
+        female: 'Afua'
+        },
+        Saturday: {
+        male: 'Kwame',
+        female: 'Ama'
+        },
+        } 
+    var date=[year,month,day].join("/");
+    var date=new Date(date);
+    var dayOfweekBorn=date.getDay();
+    var dayOfTheWeek=Object.keys(names);
+    var ghanaNames=names[daysOfTheWeek[dayOfweekBorn]][gender];
+    console.log(ghanaName);
 }
